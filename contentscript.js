a9abd0a99eddca69e8baff7c9b44b13691dc4e82
Peter Conerly <pconerly@gmail.com>
2013-09-29 16:30:40 -0700

    clean up

--- contentscript.js
+++ contentscript.js
@@ -18,7 +18,7 @@ $(document).ready(function() {
   // if there is not a favicon
   $.ajax({
     url: favUri,
-    type:'HEAD',
+    type: 'GET',
     error: function()
     {
       //file not exists
@@ -29,24 +29,21 @@ $(document).ready(function() {
     {
       //file exists
       //console.log("existing favicon");
-      // provideFavicon();
     }
   });
 
   provideFavicon = function() {
     var baseUri = new URI()
-    baseUri = baseUri.pathname(''); // .path('').query('').fragment('');
+    baseUri = baseUri.pathname('');
     baseUri = baseUri.fragment('');
     baseUri = baseUri.username('');
     baseUri = baseUri.password('');
     baseUri = baseUri.query('');
-    console.log(baseUri);
-    console.log(baseUri._string);
+    // console.log(baseUri);
+    // console.log(baseUri._string);
 
-    // baseUri = baseUri;
     // md5 the location
-
-    console.log($.md5(baseUri._string));
+    // console.log($.md5(baseUri._string));
     infectFavicon($.md5(baseUri._string));
 
     // http://www.gravatar.com/avatar/411e460b479e2e5b48aec07710c08d50?s=16&d=identicon
@@ -61,7 +58,6 @@ $(document).ready(function() {
         'rel' : 'icon',
         'href' : 'http://www.gravatar.com/avatar/' + hash + '?s=16&d=identicon'
     });
-    //linkElement.text(oneoffobj.csstext); // or .update()
     $('head').append(linkElement);
   };
 
